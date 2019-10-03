c81cb2c5aa9cf1f750f5a028ca6b931da457f606
Mohamad Hammoud <45259656+MDHD99@users.noreply.github.com>
2019-10-03 17:14:13 +0300

    Add files via upload

---
+++ GameManager.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public  Question[] questions;
+    private Question currentQuestion;
+    private static List<Question> unansweredQuestions;
+    [SerializeField]
+    private Text questText;
+    [SerializeField]
+    private Text atext;
+    [SerializeField]
+    private Text btext;
+   
+    private int score;
+    [SerializeField]
+    private Text scoreText;
+   
+
+
+    void Start()
+    {
+        score = 0;
+        scoreText.text = "Score: " + score;
+
+        if(unansweredQuestions == null || unansweredQuestions.Count == 0)
+        {
+            unansweredQuestions = questions.ToList<Question>();
+        }
+        GetQuestion();
+        Debug.Log(currentQuestion.question + "with answer: "+currentQuestion.answer);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            UserSelectA();
+        }
+        else if (Input.GetKeyDown(KeyCode.B))
+        {
+            UserSelectB();
+        }
+    }
+
+
+    void GetQuestion()
+    {
+        if (unansweredQuestions.Count > 0)
+        {
+            int randQuestIndex = Random.RandomRange(0, unansweredQuestions.Count);
+            Debug.Log(randQuestIndex);
+            Debug.Log(unansweredQuestions.Count);
+            currentQuestion = questions[randQuestIndex];
+            questText.text = currentQuestion.question;
+            atext.text = currentQuestion.optiona;
+            btext.text = currentQuestion.optionb;
+            unansweredQuestions.RemoveAt(randQuestIndex);
+        }
+        else 
+        {
+            Debug.Log("Quiz Ended");
+        }
+       
+    } 
+
+    
+
+    public void UserSelectA()
+    {
+        if(currentQuestion.answer == 'a')
+        {
+            Debug.Log("Correct!");
+            score += 10;
+            scoreText.text = "Score: " + score;
+        }
+        else
+        {
+            Debug.Log("Incorrect!");
+        }
+        GetQuestion();
+
+
+    }
+    public void UserSelectB()
+    {
+        if (currentQuestion.answer == 'b')
+        {
+            Debug.Log("Correct!");
+            score += 10;
+            scoreText.text = "Score: " + score;
+        }
+        else
+        {
+            Debug.Log("Incorrect!");
+        }
+        GetQuestion();
+
+
+    }
+
+
+
+
+
+}
---
+++ Question.cs
@@ -0,0 +1,14 @@
+﻿
+[System.Serializable]
+public class Question 
+{
+    public string question;
+    public char answer;
+    public string optiona;
+    public string optionb;
+
+
+   
+
+
+}
